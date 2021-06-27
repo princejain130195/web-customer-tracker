1635ce5a94261f6ada17d630bc11d69ff1d5844d
Prince <princejain130195@gmail.com>
2021-06-27 20:51:50 +0530

    Added Service layer in Customer project

--- src/com/luv2code/springdemo/Controller/CustomerController.java
+++ src/com/luv2code/springdemo/Controller/CustomerController.java
@@ -4,22 +4,23 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.luv2code.spring.entity.Customer;
-import com.luv2code.springdemo.DAO.CustomerDAO;
+import com.luv2code.springdemo.service.CustomerService;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 	
 	@Autowired
-	private CustomerDAO customerDAO; 
+	private CustomerService customerService;
 	
-	@RequestMapping("/list")
+	@GetMapping("/list")
 	public String listCustomer(Model model) {
 		
-		List<Customer> customerList = customerDAO.getCustomers();
+		List<Customer> customerList = customerService.getCustomers();
 		
 		model.addAttribute("customers", customerList);
 		
